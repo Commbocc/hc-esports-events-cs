b7788798617f6fd52692d9969a7a3173db778148
Jordan Pisani <pisanij@hillsboroughcounty.org>
2022-09-06 16:34:57 -0400

    add responsive table

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -8,30 +8,32 @@ const smAndLarger = breakpoints.greater("sm");
 </script>
 
 <template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Tournament/Event</th>
-        <th>Date</th>
-        <th>Location</th>
-        <th>Description</th>
-        <th>Registration Link</th>
-      </tr>
-    </thead>
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Tournament/Event</th>
+          <th>Date</th>
+          <th>Location</th>
+          <th>Description</th>
+          <th>Registration Link</th>
+        </tr>
+      </thead>
 
-    <tbody>
-      <tr v-for="(event, i) in events.data" :key="i">
-        <td>{{ event.fields.Name }}</td>
-        <td>
-          {{ new Date(event.fields.DateTime).toLocaleDateString("en-US") }},
-          {{ new Date(event.fields.DateTime).toLocaleTimeString("en-US") }}
-        </td>
-        <td>{{ event.fields.LocationName[0] }}</td>
-        <td>{{ event.fields.Description }}</td>
-        <td>
-          <a href="{{event.fields.Link}}">{{ event.fields.Link }}</a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+      <tbody>
+        <tr v-for="(event, i) in events.data" :key="i">
+          <td>{{ event.fields.Name }}</td>
+          <td>
+            {{ new Date(event.fields.DateTime).toLocaleDateString("en-US") }},
+            {{ new Date(event.fields.DateTime).toLocaleTimeString("en-US") }}
+          </td>
+          <td>{{ event.fields.LocationName[0] }}</td>
+          <td>{{ event.fields.Description }}</td>
+          <td>
+            <a href="{{event.fields.Link}}">{{ event.fields.Link }}</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
