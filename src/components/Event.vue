204eda957733cc0f0aed168a15f5f3dfb9c3ef8b
Jordan Pisani <pisanij@hillsboroughcounty.org>
2022-09-06 15:06:54 -0400

    init app

---
+++ src/components/Event.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { breakpointsBootstrapV5, useBreakpoints } from "@vueuse/core";
+import { events } from "../lib/events";
+
+const breakpoints = useBreakpoints(breakpointsBootstrapV5);
+
+const smAndLarger = breakpoints.greater("sm");
+</script>
+
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Tournament/Event</th>
+        <th>Date</th>
+        <th>Location</th>
+        <th>Description</th>
+        <th>Registration Link</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(event, i) in events.data" :key="i">
+        <td>{{ event.fields.Name }}</td>
+        <td>
+          {{ new Date(event.fields.DateTime).toLocaleDateString("en-US") }},
+          {{ new Date(event.fields.DateTime).toLocaleTimeString("en-US") }}
+        </td>
+        <td>{{ event.fields.LocationName[0] }}</td>
+        <td>{{ event.fields.Description }}</td>
+        <td>
+          <a href="{{event.fields.Link}}">{{ event.fields.Link }}</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+interface Event {
+  id: string;
+  createdTime: string;
+  fields: {
+    Name: string;
+    DateTime: Date;
+    LocationName: string;
+    Description?: string;
+    Link?: string;
+  };
+}
